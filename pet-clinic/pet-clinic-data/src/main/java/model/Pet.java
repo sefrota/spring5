ac245c9938c0a1c8e01bfd31bccd48a3becb7d21
sefrota <miguel265>
2019-02-07 23:25:35 +0000

    Refactored to multi module. Closes #1

---
+++ pet-clinic/pet-clinic-data/src/main/java/model/Pet.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by sergioletras on 07/02/19.
+ */
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate localDate;
+
+    public Pet(PetType petType, Owner owner, LocalDate localDate) {
+        this.petType = petType;
+        this.owner = owner;
+        this.localDate = localDate;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+}
---
+++ pet-clinic/pet-clinic-web/src/test/java/com/sletras/PetClinicApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sletras;
+
+import com.sletras.PetClinicApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PetClinicApplication.class)
+public class PetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
