ec4c782fac1bc3af8935b810808c220e5d3b344c
sefrota <miguel265>
2019-02-18 22:26:35 +0000

    Added Vet List. Closes #15.

--- pet-clinic/pet-clinic-web/src/main/java/com/sletras/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/sletras/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.sletras.controllers;
 
+import com.sletras.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping(path = {"vets", "vets.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
