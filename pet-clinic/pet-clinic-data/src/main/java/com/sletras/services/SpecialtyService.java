b9c2bf017a3c876b4aa841ef7699c587960666c4
sefrota <miguel265>
2019-02-25 23:24:21 +0000

    Created Specialty Map Service. Closes #31

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/sletras/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package com.sletras.services;
+
+import com.sletras.model.Specialty;
+
+/**
+ * Created by sergioletras on 25/02/19.
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/com/sletras/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,10 @@
+package com.sletras.services.map;
+
+import com.sletras.model.Specialty;
+import com.sletras.services.SpecialtyService;
+
+/**
+ * Created by sergioletras on 25/02/19.
+ */
+public class SpecialtyServiceMap extends BaseEntityServiceMap<Specialty, Long> implements SpecialtyService {
+}
